ae6e7a1055980f17efe59a158f9993304ea7c731
jcailly <jeremy.cailly@hotmail.fr>
2024-09-20 09:50:21 +0000

    sidebar componented

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,24 @@
-// place files you want to import through the `$lib` alias in this folder.
+export async function fetchUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    return await response.json();
+}
+
+export async function fetchAlbums() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+    return await response.json();
+}
+
+export async function fetchPhotos() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    return await response.json();
+}
+
+export async function fetchPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return await response.json();
+}
+
+export async function fetchComments() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+    return await response.json();
+}
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,1 +1,186 @@
-<h1>Welcome to the user page !</h1>
+<script>
+    import { goto } from '$app/navigation'; // Import the SvelteKit navigation function
+    import Sidebar from '../Sidebar.svelte';
+    import { browser } from '$app/environment'; // Import to check for browser environment
+
+    let users = [];
+    let posts = [];
+    let comments = [];
+    let selectedPostComments = [];
+    let albums = [];
+    let photos = [];
+    let cookieValue = null;
+
+    // Fetch data when the component is mounted
+    const fetchUsers = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        users = await response.json();
+    };
+
+    const fetchPosts = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        posts = await response.json();
+        console.log("posts: ", posts);
+    };
+
+    const fetchComments = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        comments = await response.json();
+        console.log("comments: ", comments);
+    };
+
+    const fetchAlbums = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+        albums = await response.json();
+    };
+
+    const fetchPhotos = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+        photos = await response.json();
+    };
+
+    function getCookieValue() {
+        if (browser) {
+            // Ensure the document is only accessed in the browser
+            let theCookies = document.cookie.split(";"); // Split cookies by semicolon
+            for (let i = 0; i < theCookies.length; i++) {
+                let cookie = theCookies[i].trim(); // Remove leading spaces
+                console.log("cookie is:", cookie);
+                if (cookie.startsWith("sessionId=")) {
+                    return cookie.substring("sessionId=".length); // Get the value after "sessionId="
+                }
+            }
+        }
+        return null; // Return null if sessionId cookie is not found
+    }
+
+    cookieValue = getCookieValue();
+    console.log("Value of the cookie:", cookieValue);
+
+    async function fetchCookieValue() {
+        if (!cookieValue) {
+            return;
+        }
+        try {
+            const response = await fetch('http://localhost:8080/verificationSessionId', {
+                method: 'POST',
+                body: JSON.stringify(cookieValue)
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                if (result.hasOwnProperty("Success")) {
+                    console.log('File and data uploaded successfully:', result);
+                } else {
+                    goto('/');
+                }
+            } else {
+                console.error('Cookie not valid');
+            }
+        } catch (error) {
+            console.error('Error with the cookie:', error);
+        }
+    }
+
+    // Fetch data after checking for browser environment
+    if (browser) {
+        fetchCookieValue();
+        fetchUsers();
+        fetchPosts();
+        fetchComments();
+        fetchAlbums();
+        fetchPhotos();
+    }
+
+    let previousUserId = null;
+
+    const findUserName = (userId) => {
+        const user = users.find(user => user.id === userId);
+        return user ? user.name : 'Unknown User';
+    };
+
+    const findCommentPosts = (postId) => {
+        return comments.filter(comment => comment.postId === postId);
+    };
+
+    function onClick(event) {
+        event.preventDefault();
+        console.log("Hello");
+    }
+
+    function displaySectionComment(postId) {
+        selectedPostComments = findCommentPosts(postId);
+        if (browser) {
+            let sectionComment = document.getElementById("commentSection");
+            sectionComment.style.display = "flex";
+        }
+    }
+
+    function hideSectionComment() {
+        if (browser) {
+            let sectionComment = document.getElementById("commentSection");
+            sectionComment.style.display = "none";
+            selectedPostComments = [];
+        }
+    }
+</script>
+ 
+ 
+ <section class="flex min-h-screen bg-gray-100">
+    <Sidebar { users } { albums } { photos } />
+     <section class="flex min-h-screen w-full">
+         <section class="flex flex-col bg-gray-200 min-h-screen">
+                 <section>
+                 <textarea class="h-7 w-96 m-2" placeholder="Ecrire le contenu du post"></textarea>
+                     <div class="m-2.5 h-screen overflow-auto">
+                         <!-- Render the fetched posts -->
+                         {#if posts.length > 0}
+                             {#each posts as post}
+                                 <button on:click={displaySectionComment(post.id)} id="postDiv" class="flex flex-col bg-white h-auto p-2 mt-4 mb-4 hover:scale-103 w-full">
+                                     <div class="flex flex-row justify-around p-4">
+                                         <div>Username: {findUserName(post.userId)}</div>
+                                         <div class="flex">
+                                             <div class="w-4 h-5 bg-red-500"></div>
+                                             <div class="w-4 h-5 bg-red-900"></div>
+                                         </div>
+                                         <div class="flex">
+                                             <div class="w-4 h-5 bg-red-500"></div>
+                                             <div class="w-4 h-5 bg-red-900"></div>
+                                         </div>
+                                         <!-- <div>Post ID: {post.id}</div> -->
+                                     </div>
+                                     <div class="flex p-2">Title: {post.title}</div>
+                                     <div class="flex bg-gray-300 p-4">Body: {post.body}</div>
+                                 </button>
+                             {/each}
+                         {:else}
+                         <p>Loading...</p>
+                         {/if}
+                     </div>
+                 </section>
+         </section>
+          <section id="commentSection" class="bg-gray-600 hidden flex-col min-h-screen w-[320px] max-w-[320px]">
+             
+             <div class="h-screen overflow-auto">
+                 <button on:click={hideSectionComment}>X</button>
+ 
+                 <div>
+                     <!-- Render the comments for the selected post -->
+                     {#if selectedPostComments.length > 0}
+                         {#each selectedPostComments as comment}
+                             <div class="flex flex-col bg-white h-auto p-1 m-3 text-xs text-center">
+                                 <div class="flex flex-col justify-around p-4">
+                                     <div><strong>Name:</strong> {comment.name}</div>
+                                     <div><strong>Email:</strong> {comment.email}</div>
+                                     <div><strong>Body:</strong> {comment.body}</div>
+                                 </div>
+                             </div>
+                         {/each}
+                     {:else}
+                         <p>No comments available for this post.</p>
+                     {/if}
+                 </div>
+             </div>
+         </section>
+     </section>
+ </section>
